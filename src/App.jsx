2b414af0523c9e707951b3b3a130b874317ce25b
analistamlc <122987725+analistamlc@users.noreply.github.com>
2024-09-15 18:23:44 -0500

    Inicalizacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import Logo from '/Logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Project init</h1>
+      /</div> 
+    </>
+  )
+}
+
+export default App
